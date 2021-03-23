fdceb76766ce9bcaabc4b6c308635c9d0d04dc0e
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-23 13:57:52 +0000

    Added utils + yargs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const yargs = require("yargs");
+
+const { addNotes } = require("../utils/notes");
+
+const command = process.argv[2];
+
+if (command == "add") {
+  console.log("adding a note...");
+  addNotes(yargs.argv.note);
+} else if (command == "remove") {
+  console.log("removing note");
+} else if (command == "list") {
+  console.log("showing all notes");
+} else {
+  console.log("command not found");
+}
